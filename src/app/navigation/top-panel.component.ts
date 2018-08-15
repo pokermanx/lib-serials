50b50a8da2472061fcf615558885e7e90cb809f7
Oleksandr Marushchak <35522827+noxios5674@users.noreply.github.com>
2018-08-15 16:52:45 +0300

    Update 3.1
    Fix toggle

--- src/app/navigation/top-panel.component.ts
+++ src/app/navigation/top-panel.component.ts
@@ -17,10 +17,12 @@ export class TopPanelComponent implements OnInit {
 
   onDarkTheme(){
     if(this.darkTheme === false){
-      document.getElementsByTagName('html')[0].classList.replace('ligth-theme', 'dark-theme'); 
+      document.getElementsByTagName('html')[0].className = '';
+      document.getElementsByTagName('html')[0].classList.add('dark-theme');
       this.darkTheme = true;
     } else {
-      document.getElementsByTagName('html')[0].classList.replace('dark-theme', 'ligth-theme');
+      document.getElementsByTagName('html')[0].className = '';
+      document.getElementsByTagName('html')[0].classList.add('ligth-theme');
       this.darkTheme = false;
     }
   }
